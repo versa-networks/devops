c7c240bba47d69a9d16be361b5009ee869eb0d30
maksymdmitriiev <maksym@versa-networks.com>
2024-11-28 14:34:07 +0100

    Fetch_interface_details adjusted with BUG fixes

--- python/Fetch_Interfaces_details/fetch_int_details.py
+++ python/Fetch_Interfaces_details/fetch_int_details.py
@@ -13,6 +13,7 @@ import urllib3
 import os 
 from pathlib import Path
 import time
+import ipaddress
 
 
 #suppress certificate verification warnings to accept self signed certificate
@@ -59,13 +60,26 @@ def is_destinationVD_valid(destinationVD):
 def appliance_from_json(json_appliance):
     device_name = json_appliance['name']
     device_uuid = json_appliance['uuid']
-    device_serial = json_appliance['Hardware']['serialNo']
-    device_location = json_appliance['location']
-    device_hardware = json_appliance['Hardware']['model']
+    device_location = str
+    device_serial : str
+    device_hardware : str
+    if 'Hardware' in json_appliance: 
+        if 'serialNo' in json_appliance['Hardware']: device_serial = json_appliance['Hardware']['serialNo'] 
+        else: device_serial = "No Serial Number"
+        if 'model' in json_appliance['Hardware']: device_hardware = json_appliance['Hardware']['model']
+        else: device_hardware = "Can't identify hardware type" 
+    else:  
+        device_serial = "No Serial Number" 
+        device_hardware = "Can't identify hardware type"
+    if 'location' in json_appliance:
+        device_location = json_appliance['location']
+    else:
+        device_location = "Can't identify location"
     return ApplianceNameUuid(device_name,device_uuid,device_serial,device_location,device_hardware)
 
 urls_list = []
 appliance_list = []
+failed_connection = []
 
 #iterate accross devices and get serial number, hardware model and location information. Generate list of appliances with inner list of appliance details
 def appliance_objects(appliances):
@@ -91,6 +105,46 @@ def appliances_url(appliances):
         appliance_list.append(appliance.device_name)
     return urls_list, appliance_list
 
+#Collect VRRP IP Adresses from interfaces
+def appliances_VRRP_IP_list(appliances, failed_connection):
+#Create a loop to generate a list of URLs that can be used to get VRRP IP    
+    VRRP_url = {}
+    VRRP_IPs = {}
+    print('Preparing to start VRRP IPs collection')
+    i = 0
+    while i < 10:
+        i = i + 1
+        print(f'{i}0%', end='\r')
+        time.sleep(1) 
+    print('\n')
+    for json_appliance in appliances:
+        appliance = appliance_from_json(json_appliance)
+        url_appliance_VRRP = f'https://{args.destinationVD}:9182/vnms/dashboard/appliance/{appliance.device_name}/live?command=vrrp%2Fgroup%2Forg%2F{args.tenant}%2Fsummary?deep=true'
+        VRRP_url.update({appliance.device_name:url_appliance_VRRP})
+
+    for device_name, connect_url in VRRP_url.items():
+        if device_name in failed_connection:
+            print(f'Skipping connection to {device_name} as it is not reachable')
+            continue
+        VRRP_response = requests.get(connect_url, verify=False,timeout=20,auth=requests.auth.HTTPBasicAuth(args.username,args.password))
+        if VRRP_response.status_code == 504:
+            print(f'ATTENTION! {device_name} didn\'t get VRRP IP due to connection timeout')
+            continue 
+        if VRRP_response.text.strip():
+            VRRP_response_json = VRRP_response.json()
+
+            for entry in VRRP_response_json['collection']['interfaces:summary']:
+                VRRP_IP = entry.get('virtual-ip')
+                if VRRP_IP:
+                    if device_name not in VRRP_IPs:
+                        VRRP_IPs[device_name] = []
+                    VRRP_IPs[device_name].append(VRRP_IP)
+            print(f'VRRP IPs saved for {device_name}')
+        else: print(f'{device_name} has no VRRP')    
+        time.sleep(2)
+    print('VRRP IP address inventory collection completed')
+    return VRRP_IPs
+
 #run APIs to Director to get device interface details
 def appliances_interfaces(urls_list):
     all_appliances_interfaces = []
@@ -100,6 +154,7 @@ def appliances_interfaces(urls_list):
             json_response = requests.get(json_url, verify=False,timeout=20,auth=requests.auth.HTTPBasicAuth(args.username,args.password))
             if json_response.status_code == 504:
                 print(f"504 Gateway Timeout for device: {appliance_name}. Appliance is not reachable.")
+                failed_connection.append(appliance_name)
                 continue  
             json_data = json_response.json()
             current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -124,12 +179,12 @@ def appliances_interfaces(urls_list):
             
             all_appliances_interfaces.append(appliance_name)
             all_appliances_interfaces.append(json_data)
-
+            
         except FileNotFoundError:
             print(f"File not found: {inventory_file}. Creating a new file.")
             with open(inventory_file, 'w') as json_file:
                 json.dump([], json_file)
-        time.sleep(1)
+        time.sleep(2)
     return all_appliances_interfaces
 
 
@@ -163,13 +218,14 @@ def parse_appliance_details(all_appliance_interfaces, appliance_object_list):
                         interface_public_ips.append('No Public IP')  # Handle cases where there is no address, but put no Public IP instead
         
         if device_name is not None and interface_names:
-            device_dict = {'Device Name': device_name, 'Device Hardware Module': device_hardware, 'Device Serial Number': device_serial, 'Device Location Info': device_location}
+            device_dict = {'Device Name': device_name, 'Device Hardware Model': device_hardware, 'Device Serial Number': device_serial, 'Device Location Info': device_location}
 
             for interface, ip, public_ip in zip(interface_names, interface_ips, interface_public_ips):
                 device_dict[f"{interface}__IP"] = ip
                 device_dict[f"{interface}_Public_IP"] = public_ip
             
             csv_data.append(device_dict)
+    
             interface_ips = None
             interface_public_ips = None
             device_name = None
@@ -179,14 +235,43 @@ def parse_appliance_details(all_appliance_interfaces, appliance_object_list):
         for device in csv_data:
             device_exist_name = device['Device Name']
             if device_object_name == device_exist_name and len(device_object) == 4:    
-                device['Device Hardware Module'] = device_object[1]
+                device['Device Hardware Model'] = device_object[1]
                 device['Device Serial Number'] = device_object[2]
                 device['Device Location Info'] = device_object[3]
+    #Add VRRP IP addresses for all devices if presented
+    list_VRRP_IP = appliances_VRRP_IP_list(appliances, failed_connection)
+    for device in csv_data:
+        device_object_name = device['Device Name']
+        VRRP_IPs = []
+        for entry in list_VRRP_IP:
+            if entry == device_object_name:
+                VRRP_IPs.extend(list_VRRP_IP[entry])
+            if VRRP_IPs:
+                VRRP_IPs = list(set(VRRP_IPs))
+                device['Virtual IP address (VRRP)'] = ', '.join(map(str, VRRP_IPs))
     return csv_data
 
 #remove subnets for interface IP addresses
 def remove_subnet(ip_list):
     return [ip.split('/')[0] for ip in ip_list]
+
+#Get Private interface IPs:
+def select_versa_Private_IPs(ip_list):
+    private_v_ip_list = []   
+    for ip in ip_list:
+        ip_addr = ipaddress.IPv4Address(ip)
+        if ip_addr.is_private:
+            private_v_ip_list.append(ip)
+    return private_v_ip_list
+
+#Get Public interface IPs:
+def select_versa_Public_IPs(ip_list):
+    public_v_ip_list = []   
+    for ip in ip_list:
+        ip_addr = ipaddress.IPv4Address(ip)
+        if not ip_addr.is_private:
+            public_v_ip_list.append(ip)
+    return public_v_ip_list
 
 
 #save csv_data in csv file            
@@ -194,11 +279,11 @@ def get_csv (csv_data):
     interface_keys = set()
     for entry in csv_data:
         for key in entry.keys():
-            if key != "Device Name" and key != "Device Hardware Module" and key != "Device Location Info" and key != "Device Serial Number":
+            if key not in ["Device Name", "Device Hardware Model", "Device Location Info", "Device Serial Number", "Virtual IP address (VRRP)"]:
                 interface_keys.add(key)
     interface_keys = sorted(interface_keys, key=lambda x: (x.split('_')[-1] != "Public_IP", x.split('_')))
     #Define CSV columns
-    columns = ["Device Name", "Device Hardware Module", "Device Serial Number", "Device Location Info", "All Versa interfaces IPs", "All Public/Outside IPs (detected by software)"] + interface_keys
+    columns = ["Device Name", "Device Hardware Model", "Device Serial Number", "Device Location Info", "Virtual IP address (VRRP)", "All Versa interfaces Private IPs", "All Versa interfaces Public IPs", "All Public/Outside IPs (detected by VOS)"] + interface_keys
     #Write data to CSV file
     current_directory = os.path.dirname(os.path.abspath(__file__))
     csv_file_path = os.path.join(current_directory, f"{args.tenant}_Inventory.csv")
@@ -209,9 +294,13 @@ def get_csv (csv_data):
             row = {key: entry.get(key, "") for key in columns}
             all_ips = [entry[key] for key in interface_keys if "__IP" in key and entry.get(key)]
             all_ips_no_subnet = remove_subnet(all_ips)
+            all_v_private_ips_no_subnet = select_versa_Private_IPs(all_ips_no_subnet)
+            all_v_private_ips_no_subnet_no_duplicates = list(set(all_v_private_ips_no_subnet))
+            all_v_public_ips_no_subnet = select_versa_Public_IPs(all_ips_no_subnet)
             all_public_ips = [entry[key] for key in interface_keys if "_Public_IP" in key and entry.get(key) and entry[key] != "No Public IP"]
-            row["All Versa interfaces IPs"] = ", ".join(all_ips_no_subnet)
-            row["All Public/Outside IPs (detected by software)"] = ", ".join(all_public_ips)
+            row["All Versa interfaces Private IPs"] = ", ".join(all_v_private_ips_no_subnet_no_duplicates)
+            row["All Versa interfaces Public IPs"] = ", ".join(all_v_public_ips_no_subnet)
+            row["All Public/Outside IPs (detected by VOS)"] = ", ".join(all_public_ips)
             writer.writerow(row)
     print(f"{args.tenant}_Inventory.csv file has been created in {current_directory}.")
 
