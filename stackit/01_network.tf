27b6809ac87a65b9ae3d66a151ad9894b2be6142
Ravi Kakadia <ravi.k@versa-networks.com>
2025-11-21 06:05:19 +0000

    updating files as per testing

--- stackit/01_network.tf
+++ stackit/01_network.tf
@@ -11,10 +11,9 @@ data "stackit_network" "versa-mgmt-network" {
 resource "stackit_network_interface" "lan1" {
   project_id         = var.project_id
   network_id         = data.stackit_network.versa-network.network_id
-  security           = true
+  security           = false
   name               = "LAN1"
-  ipv4               = "x.x.x.x"
-  security_group_ids = [stackit_security_group.sg1.security_group_id]
+  ipv4               = "10.0.1.11"
 }
 
 resource "stackit_network_interface" "lan2" {
@@ -22,7 +21,7 @@ resource "stackit_network_interface" "lan2" {
   network_id = data.stackit_network.versa-mgmt-network.network_id
   security   = false
   name       = "LAN2"
-  ipv4       = "y.y.y.y"
+  ipv4       = "192.168.1.11"
 }
 
 resource "stackit_network_interface" "lan3" {
@@ -30,7 +29,7 @@ resource "stackit_network_interface" "lan3" {
   network_id = data.stackit_network.versa-network.network_id
   security   = false
   name       = "LAN3"
-  ipv4       = "a.a.a.a"
+  ipv4       = "10.0.1.12"
 }
 
 resource "stackit_network_interface" "lan4" {
@@ -38,14 +37,162 @@ resource "stackit_network_interface" "lan4" {
   network_id = data.stackit_network.versa-mgmt-network.network_id
   security   = false
   name       = "LAN4"
-  ipv4       = "b.b.b.b"
+  ipv4       = "192.168.1.12"
 }
 
-resource "stackit_public_ip" "server1_public_ip" {
+resource "stackit_network_interface" "lan5" {
+  project_id = var.project_id
+  network_id = data.stackit_network.versa-network.network_id
+  security   = false
+  name       = "LAN3"
+  ipv4       = "10.0.1.13"
+}
+
+resource "stackit_network_interface" "lan6" {
+  project_id = var.project_id
+  network_id = data.stackit_network.versa-mgmt-network.network_id
+  security   = false
+  name       = "LAN4"
+  ipv4       = "192.168.1.13"
+}
+
+resource "stackit_network_interface" "lan7" {
+  project_id = var.project_id
+  network_id = data.stackit_network.versa-network.network_id
+  security   = false
+  name       = "LAN7"
+  ipv4       = "10.0.1.14"
+}
+
+resource "stackit_network_interface" "lan8" {
+  project_id = var.project_id
+  network_id = data.stackit_network.versa-mgmt-network.network_id
+  security   = false
+  name       = "LAN8"
+  ipv4       = "192.168.1.14"
+}
+
+resource "stackit_network_interface" "lan9" {
+  project_id = var.project_id
+  network_id = data.stackit_network.versa-network.network_id
+  security   = false
+  name       = "LAN9"
+  ipv4       = "10.0.1.15"
+}
+
+resource "stackit_network_interface" "lan10" {
+  project_id = var.project_id
+  network_id = data.stackit_network.versa-mgmt-network.network_id
+  security   = false
+  name       = "LAN10"
+  ipv4       = "192.168.1.15"
+}
+
+resource "stackit_network_interface" "lan11" {
+  project_id         = var.project_id
+  network_id         = data.stackit_network.versa-network.network_id
+  security           = false
+  name               = "LAN11"
+  ipv4               = "10.0.1.16"
+}
+
+resource "stackit_network_interface" "lan12" {
+  project_id         = var.project_id
+  network_id         = data.stackit_network.versa-mgmt-network.network_id
+  security           = false
+  name               = "LAN12"
+  ipv4               = "192.168.1.16"
+}
+
+resource "stackit_network_interface" "lan13" {
+  project_id         = var.project_id
+  network_id         = data.stackit_network.versa-network.network_id
+  security           = false
+  name               = "LAN13"
+  ipv4               = "10.0.1.17"
+}
+
+resource "stackit_network_interface" "lan14" {
+  project_id         = var.project_id
+  network_id         = data.stackit_network.versa-mgmt-network.network_id
+  security           = false
+  name               = "LAN14"
+  ipv4               = "192.168.1.17"
+}
+
+resource "stackit_network_interface" "lan15" {
+  project_id         = var.project_id
+  network_id         = data.stackit_network.versa-network.network_id
+  security           = false
+  name               = "LAN15"
+  ipv4               = "10.0.1.18"
+}
+
+resource "stackit_network_interface" "lan16" {
+  project_id = var.project_id
+  network_id = data.stackit_network.versa-mgmt-network.network_id
+  security   = false
+  name       = "LAN16"
+  ipv4       = "192.168.1.18"
+}
+
+resource "stackit_network_interface" "lan17" {
+  project_id         = var.project_id
+  network_id         = data.stackit_network.versa-network.network_id
+  security           = false
+  name               = "LAN17"
+  ipv4               = "10.0.1.19"
+}
+
+resource "stackit_network_interface" "lan18" {
+  project_id = var.project_id
+  network_id = data.stackit_network.versa-mgmt-network.network_id
+  security   = false
+  name       = "LAN18"
+  ipv4       = "192.168.1.19"
+}
+
+resource "stackit_network_interface" "lan19" {
+  project_id         = var.project_id
+  network_id         = data.stackit_network.versa-network.network_id
+  security           = false
+  name               = "LAN19"
+  ipv4               = "10.0.1.20"
+}
+
+resource "stackit_network_interface" "lan20" {
+  project_id = var.project_id
+  network_id = data.stackit_network.versa-mgmt-network.network_id
+  security   = false
+  name       = "LAN20"
+  ipv4       = "192.168.1.20"
+}
+
+resource "stackit_network_interface" "lan21" {
+  project_id         = var.project_id
+  network_id         = data.stackit_network.versa-network.network_id
+  security           = false
+  name               = "LAN21"
+  ipv4               = "10.0.1.21"
+}
+
+resource "stackit_network_interface" "lan22" {
+  project_id = var.project_id
+  network_id = data.stackit_network.versa-mgmt-network.network_id
+  security   = false
+  name       = "LAN22"
+  ipv4       = "192.168.1.21"
+}
+
+resource "stackit_public_ip" "director_public_ip" {
   project_id           = var.project_id
-  network_interface_id = stackit_network_interface.lan1.network_interface_id
+  network_interface_id = stackit_network_interface.lan5.network_interface_id
 }
 
+# resource "stackit_public_ip" "analytics-1-public-ip" {
+#  project_id           = var.project_id
+#  network_interface_id = stackit_network_interface.lan1.network_interface_id
+#}
 # resource "stackit_public_ip" "server2_public_ip" {
 #     project_id = var.project_id
 #     network_interface_id = stackit_network_interface.lan2.network_interface_id
--- stackit/02_vm.tf
+++ stackit/02_vm.tf
@@ -3,12 +3,12 @@ resource "stackit_server" "server1" {
   boot_volume = {
     size                  = 100
     source_type           = "image"
-    source_id             = "<image id>"
-    performance_class     = "storage_premium_perf0"
+    source_id             = "716ccaf5-0e4d-49a8-8931-4c1ac4d350d0"
+    performance_class     = "storage_premium_perf14"
     delete_on_termination = true
   }
   availability_zone  = "eu01-1"
-  name               = "versa-mgmt-nic-server1"
+  name               = "versa-analytics-analytics-node-1"
   machine_type       = var.flavor
   network_interfaces = [stackit_network_interface.lan1.network_interface_id, stackit_network_interface.lan2.network_interface_id]
 }
@@ -18,12 +18,147 @@ resource "stackit_server" "server2" {
   boot_volume = {
     size                  = 100
     source_type           = "image"
-    source_id             = "<image id>"
+    source_id             = "716ccaf5-0e4d-49a8-8931-4c1ac4d350d0"
+    performance_class     = "storage_premium_perf14"
+    delete_on_termination = true
+  }
+  availability_zone  = "eu01-1"
+  name               = "versa-analytics-search-node-1"
+  machine_type       = var.flavor
+  network_interfaces = [stackit_network_interface.lan3.network_interface_id, stackit_network_interface.lan4.network_interface_id]
+}
+
+resource "stackit_server" "server3" {
+  project_id = var.project_id
+  boot_volume = {
+    size                  = 150
+    source_type           = "image"
+    source_id             = "e50be773-2dde-4ff3-a574-dc0810ce27d4"
     performance_class     = "storage_premium_perf0"
     delete_on_termination = true
   }
   availability_zone  = "eu01-1"
-  name               = "versa-mgmt-nic-server2"
+  name               = "versa-director-1"
   machine_type       = var.flavor
-  network_interfaces = [stackit_network_interface.lan3.network_interface_id, stackit_network_interface.lan4.network_interface_id]
+  network_interfaces = [stackit_network_interface.lan5.network_interface_id, stackit_network_interface.lan6.network_interface_id]
+}
+
+resource "stackit_server" "server4" {
+  project_id = var.project_id
+  boot_volume = {
+    size                  = 100
+    source_type           = "image"
+    source_id             = "716ccaf5-0e4d-49a8-8931-4c1ac4d350d0"
+    performance_class     = "storage_premium_perf14"
+    delete_on_termination = true
+  }
+  availability_zone  = "eu01-1"
+  name               = "versa-analytics-search-node-2"
+  machine_type       = var.flavor
+  network_interfaces = [stackit_network_interface.lan7.network_interface_id, stackit_network_interface.lan8.network_interface_id]
+}
+
+resource "stackit_server" "server5" {
+  project_id = var.project_id
+  boot_volume = {
+    size                  = 100
+    source_type           = "image"
+    source_id             = "716ccaf5-0e4d-49a8-8931-4c1ac4d350d0"
+    performance_class     = "storage_premium_perf14"
+    delete_on_termination = true
+  }
+  availability_zone  = "eu01-1"
+  name               = "versa-analytics-analytics-node-2"
+  machine_type       = var.flavor
+  network_interfaces = [stackit_network_interface.lan9.network_interface_id, stackit_network_interface.lan10.network_interface_id]
+}
+
+resource "stackit_server" "server6" {
+  project_id = var.project_id
+  boot_volume = {
+    size                  = 200
+    source_type           = "image"
+    source_id             = "716ccaf5-0e4d-49a8-8931-4c1ac4d350d0"
+    performance_class     = "storage_premium_perf14"
+    delete_on_termination = true
+  }
+  availability_zone  = "eu01-1"
+  name               = "versa-analytics-log-forwarder-node-1"
+  machine_type       = var.flavorc2i8
+  network_interfaces = [stackit_network_interface.lan11.network_interface_id, stackit_network_interface.lan12.network_interface_id]
+}
+
+resource "stackit_server" "server7" {
+  project_id = var.project_id
+  boot_volume = {
+    size                  = 200
+    source_type           = "image"
+    source_id             = "716ccaf5-0e4d-49a8-8931-4c1ac4d350d0"
+    performance_class     = "storage_premium_perf14"
+    delete_on_termination = true
+  }
+  availability_zone  = "eu01-1"
+  name               = "versa-analytics-log-forwarder-node-2"
+  machine_type       = var.flavorc2i8
+  network_interfaces = [stackit_network_interface.lan13.network_interface_id, stackit_network_interface.lan14.network_interface_id]
+}
+
+resource "stackit_server" "server8" {
+  project_id = var.project_id
+  boot_volume = {
+    size                  = 1000
+    source_type           = "image"
+    source_id             = "716ccaf5-0e4d-49a8-8931-4c1ac4d350d0"
+    performance_class     = "storage_premium_perf14"
+    delete_on_termination = true
+  }
+  availability_zone  = "eu01-1"
+  name               = "versa-analytics-search-node-3"
+  machine_type       = var.flavor
+  network_interfaces = [stackit_network_interface.lan15.network_interface_id, stackit_network_interface.lan16.network_interface_id]
+}
+
+resource "stackit_server" "server9" {
+  project_id = var.project_id
+  boot_volume = {
+    size                  = 1000
+    source_type           = "image"
+    source_id             = "716ccaf5-0e4d-49a8-8931-4c1ac4d350d0"
+    performance_class     = "storage_premium_perf14"
+    delete_on_termination = true
+  }
+  availability_zone  = "eu01-1"
+  name               = "versa-analytics-search-node-4"
+  machine_type       = var.flavor
+  network_interfaces = [stackit_network_interface.lan17.network_interface_id, stackit_network_interface.lan18.network_interface_id]
+}
+
+resource "stackit_server" "server10" {
+  project_id = var.project_id
+  boot_volume = {
+    size                  = 200
+    source_type           = "image"
+    source_id             = "716ccaf5-0e4d-49a8-8931-4c1ac4d350d0"
+    performance_class     = "storage_premium_perf14"
+    delete_on_termination = true
+  }
+  availability_zone  = "eu01-1"
+  name               = "versa-analytics-log-forwarder-node-3"
+  machine_type       = var.flavorc2i8
+  network_interfaces = [stackit_network_interface.lan19.network_interface_id, stackit_network_interface.lan20.network_interface_id]
+}
+
+resource "stackit_server" "server11" {
+  project_id = var.project_id
+  boot_volume = {
+    size                  = 200
+    source_type           = "image"
+    source_id             = "716ccaf5-0e4d-49a8-8931-4c1ac4d350d0"
+    performance_class     = "storage_premium_perf14"
+    delete_on_termination = true
+  }
+  availability_zone  = "eu01-1"
+  name               = "versa-analytics-log-forwarder-node-4"
+  machine_type       = var.flavorc2i8
+  network_interfaces = [stackit_network_interface.lan21.network_interface_id, stackit_network_interface.lan22.network_interface_id]
 }
